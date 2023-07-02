35493d08d0ba4adff205400e7d2b8c1b1927a050
Cheruiyot Kipkemoi <d.cheru112@gmail.com>
2023-07-02 19:35:05 +0300

    ¯\_(ツ)_/¯ added a few styles

--- client/src/components/Contacts.js
+++ client/src/components/Contacts.js
@@ -41,12 +41,12 @@ const Contacts = () => {
     <div>
       <section className="mb-4">
         <h2 className="h1-responsive font-weight-bold text-center my-4">Contact us</h2>
-        <p className="text-center w-responsive mx-auto mb-5">
+        <p className="text-center w-responsive mx-auto mb-3">
           Do you have any questions? Please do not hesitate to contact us directly.
           Our team will come back to you within a matter of hours to help you.
         </p>
 
-        <div className="row">
+        <div className="row justify-content-center">
           <div className="col-md-9 mb-md-0 mb-5">
             <form id="contact-form" name="contact-form">
               <div className="row">
@@ -97,26 +97,29 @@ const Contacts = () => {
                   </div>
                 </div>
               </div>
+              <div className="text-center text-md-left">
+                <button className="btn btn-primary" onClick={handleSubmit}>
+                  Send
+                </button>
+              </div>
+              <div className="status"></div>
             </form>
-            <div className="text-center text-md-left">
-              <button className="btn btn-primary" onClick={handleSubmit}>
-                Send
-              </button>
-            </div>
-            <div className="status"></div>
           </div>
-          <div className="col-md-3 text-center">
+        </div>
+
+        <div className="row">
+          <div className="col-md-12 text-center">
             <ul className="list-unstyled mb-0">
               <li>
                 <i className="fas fa-map-marker-alt fa-2x"></i>
                 <p>124 Moi Avenue Street, Nairobi, Kenya</p>
               </li>
               <li>
-                <i className="fas fa-phone mt-4 fa-2x"></i>
+                <i className="fas fa-phone mt-2 fa-2x"></i>
                 <p>+254714883717</p>
               </li>
               <li>
-                <i className="fas fa-envelope mt-4 fa-2x"></i>
+                <i className="fas fa-envelope mt-2 fa-2x"></i>
                 <p>contact@vacationvibes.com</p>
               </li>
             </ul>
