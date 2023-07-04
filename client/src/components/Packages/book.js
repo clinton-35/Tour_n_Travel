c14319d382a521b1c7c99eb784bb9fcd62c04e7e
trevormadaramoringaschool <trevor.madara@student.moringaschool.gcom>
2023-07-04 16:16:27 +0300

    DOne

--- client/src/components/Packages/book.js
+++ client/src/components/Packages/book.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './book.css'
+import './book.css';
 
 const BookNow = () => {
   const [bookingData, setBookingData] = useState({
@@ -15,6 +15,7 @@ const BookNow = () => {
     tripPreference: '',
   });
 
+  const [errors, setErrors] = useState([]);
   const navigate = useNavigate();
 
   const handleInputChange = (event) => {
@@ -25,8 +26,6 @@ const BookNow = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      // Perform booking submission logic here
-      // You can send the bookingData to your API or perform any other necessary action
       const response = await fetch('/bookings', {
         method: 'POST',
         headers: {
@@ -34,112 +33,125 @@ const BookNow = () => {
         },
         body: JSON.stringify(bookingData),
       });
-  
+
       if (response.ok) {
-        // Redirect to a success page or display a success message
         navigate('/booking-success');
       } else {
-        // Handle error case
-        console.error('Booking submission failed');
+        const responseData = await response.json();
+        if (responseData.errors) {
+          setErrors(responseData.errors);
+        } else {
+          console.error('Booking submission failed');
+        }
       }
     } catch (error) {
       console.error('Booking submission error:', error);
     }
   };
+
   return (
     <div className='book-now-container'>
       <h2>Book Now</h2>
+      {errors.length > 0 && (
+        <div className='error-container'>
+          <ul>
+            {errors.map((error, index) => (
+              <li key={index}>{error}</li>
+            ))}
+          </ul>
+        </div>
+      )}
       <form onSubmit={handleSubmit}>
         <div className='form-group'>
-          <label htmlFor="firstName">First Name:</label>
+          <label htmlFor='firstName'>First Name:</label>
           <input
-            type="text"
-            id="firstName"
-            name="firstName"
+            type='text'
+            id='firstName'
+            name='firstName'
             value={bookingData.firstName}
             onChange={handleInputChange}
           />
         </div>
         <div>
-          <label htmlFor="lastName">Last Name:</label>
+          <label htmlFor='lastName'>Last Name:</label>
           <input
-            type="text"
-            id="lastName"
-            name="lastName"
+            type='text'
+            id='lastName'
+            name='lastName'
             value={bookingData.lastName}
             onChange={handleInputChange}
           />
         </div>
         <div>
-          <label htmlFor="email">Email:</label>
+          <label htmlFor='email'>Email:</label>
           <input
-            type="email"
-            id="email"
-            name="email"
+            type='email'
+            id='email'
+            name='email'
             value={bookingData.email}
             onChange={handleInputChange}
           />
         </div>
         <div>
-          <label htmlFor="phone">Phone:</label>
+          <label htmlFor='phone'>Phone:</label>
           <input
-            type="text"
-            id="phone"
-            name="phone"
+            type='text'
+            id='phone'
+            name='phone'
             value={bookingData.phone}
             onChange={handleInputChange}
           />
         </div>
         <div>
-          <label htmlFor="checkInDate">Check-in Date:</label>
+          <label htmlFor='checkInDate'>Check-in Date:</label>
           <input
-            type="date"
-            id="checkInDate"
-            name="checkInDate"
+            type='date'
+            id='checkInDate'
+            name='checkInDate'
             value={bookingData.checkInDate}
             onChange={handleInputChange}
           />
         </div>
         <div>
-          <label htmlFor="checkOutDate">Check-out Date:</label>
+          <label htmlFor='checkOutDate'>Check-out Date:</label>
           <input
-            type="date"
-            id="checkOutDate"
-            name="checkOutDate"
+            type='date'
+            id='checkOutDate'
+            name='checkOutDate'
             value={bookingData.checkOutDate}
             onChange={handleInputChange}
           />
         </div>
         <div>
-          <label htmlFor="adults">No. of Adults:</label>
+          <label htmlFor='adults'>No. of Adults:</label>
           <input
-            type="number"
-            id="adults"
-            name="adults"
+            type='number'
+            id='adults'
+            name='adults'
             value={bookingData.adults}
             onChange={handleInputChange}
           />
         </div>
         <div>
-          <label htmlFor="children">No. of Children:</label>
+          <label htmlFor='children'>No. of Children:</label>
           <input
-            type="number"
-            id="children"
-            name="children"
+            type='number'
+            id='children'
+            name='children'
             value={bookingData.children}
             onChange={handleInputChange}
           />
         </div>
         <div>
-          <label htmlFor="tripPreference">Trip Preference:</label>
+          <label htmlFor='tripPreference'>Trip Preference:</label>
           <textarea
-            id="tripPreference"
-            name="tripPreference"
+            id='tripPreference'
+            name='tripPreference'
             value={bookingData.tripPreference}
             onChange={handleInputChange}
           />
         </div>
-        <button type="submit">Book Now</button>
+        <button type='submit'>Book Now</button>
       </form>
     </div>
   );
