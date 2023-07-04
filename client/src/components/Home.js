c7db1f891daf75dbc9258992d30e0a034edf6de2
clinton <clinton.wambua@student.moringaschool.com>
2023-07-04 09:05:17 +0300

    chomepage

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import { Link } from "react-router-dom";
 import Carousel from "./home /Carousel";
+import Destination from "./Destinations";
 
 
 
@@ -9,16 +10,16 @@ function Home() {
   return (
     <div className="landing">
       <div className="landing row">
-        <div className="col d-flex justify-content-center align-items-center">
+      
           <Carousel />
           {/* <img
             className="image"
             src="https://media.istockphoto.com/id/1320186474/vector/music-notes-with-curves-and-swirls.jpg?s=612x612&w=0&k=20&c=neILgetK4Eq8J3Z4e-80nR82pjVSqnUUVENH90Fmt40="
             alt="image loading..."
           /> */}
-        </div>
+        
 
-        <div className="col d-flex justify-content-center align-items-center">
+        <div className="wording">
           <div>
             
             <p>
@@ -32,7 +33,7 @@ function Home() {
           </div>
         </div>
       </div>
-     
+     <Destination/>
     </div>
   );
 }
--- client/src/components/home /Carousel.js
+++ client/src/components/home /Carousel.js
@@ -1,58 +1,66 @@
-import React, { useState, useEffect } from 'react';
+import React from "react";
 
 function Carousel() {
-  const [activeIndex, setActiveIndex] = useState(0);
-
-  const images = [
-    'https://images.unsplash.com/photo-1554990772-0bea55d510d5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGtlbnlhJTIwd2lsZGxpZmV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60',
-    'https://images.unsplash.com/photo-1536797485467-3809860ee00a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGtlbnlhJTIwd2lsZGxpZmV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60',
-    'https://images.unsplash.com/photo-1664713815297-9ce06950c022?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8a2VueWElMjB3aWxkbGlmZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60',
-    'https://images.unsplash.com/photo-1664705315196-e2fae719e1e2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGtlbnlhJTIwd2lsZGxpZmV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60',
-    'https://images.unsplash.com/photo-1633345778967-3760204a4c57?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8a2VueWElMjBiZWFjaHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60',
-    'https://images.unsplash.com/photo-1489392191049-fc10c97e64b6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8a2VueWElMjBoaXN0b3JpY2FsJTIwc2l0ZXN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60',
-    'https://images.unsplash.com/photo-1524414621493-7dec026782c3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGtlbnlhJTIwaGlzdG9yaWNhbCUyMHNpdGVzfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
-  ];
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setActiveIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 2000);
-    return () => clearInterval(intervalId);
-  }, [images.length]);
-
   return (
     <div
-      id="carouselExampleAutoplaying"
-      className="carousel slide"
+      id="hero-carousel"
+      class="carousel slide"
       data-bs-ride="carousel"
     >
-      <div className="carousel-inner">
-        {images.map((image, index) => (
-          <div
-            key={index}
-            className={`carousel-item ${index === activeIndex ? 'active' : ''}`}
-          >
-            <img src={image} className="d-block w-100" alt={`Slide ${index}`} />
-          </div>
-        ))}
+      <div class="carousel-inner">
+        <div class="carousel-item active c-item" data-bs-interval="2000">
+          <img
+            src="https://images.unsplash.com/photo-1554990772-0bea55d510d5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGtlbnlhJTIwd2lsZGxpZmV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"
+            class="d-block w-100 c-img"
+            alt="slide 1"
+          />
+        </div>
+        <div class="carousel-item c-item" data-bs-interval="2000">
+          <img
+            src="https://plus.unsplash.com/premium_photo-1661963841502-6d38dee0963d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8a2VueWElMjB3aWxkbGlmZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60"
+            class="d-block w-100 c-img"
+            alt="slide 2"
+          />
+        </div>
+        <div class="carousel-item c-item">
+          <img
+            src="https://images.unsplash.com/photo-1559494487-a5bbc635ed2b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8a2VueWElMjB3aWxkbGlmZSUyMGNhcnN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"
+            class="d-block w-100 c-img"
+            alt="slide 3"
+          />
+        </div>
+        <div class="carousel-item c-item">
+          <img
+            src="https://plus.unsplash.com/premium_photo-1661846340419-89bf27138124?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8a2VueWElMjBmbGFtaW5nb3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60"
+            class="d-block w-100 c-img"
+            alt="slide 3"
+          />
+        </div>
+        <div class="carousel-item c-item">
+          <img
+            src="https://images.unsplash.com/photo-1569474028404-5eb46fd9b4fa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGtlbnlhJTIwYmVhY2hlc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60"
+            class="d-block w-100 c-img"
+            alt="slide 3"
+          />
+        </div>
       </div>
       <button
-        className="carousel-control-prev"
+        class="carousel-control-prev"
         type="button"
-        data-bs-target="#carouselExampleAutoplaying"
+        data-bs-target="#carouselExampleInterval"
         data-bs-slide="prev"
       >
-        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span className="visually-hidden">Previous</span>
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
       </button>
       <button
-        className="carousel-control-next"
+        class="carousel-control-next"
         type="button"
-        data-bs-target="#carouselExampleAutoplaying"
+        data-bs-target="#carouselExampleInterval"
         data-bs-slide="next"
       >
-        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-        <span className="visually-hidden">Next</span>
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
       </button>
     </div>
   );
