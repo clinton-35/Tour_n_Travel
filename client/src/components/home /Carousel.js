b86c57b5fa31cd57d4a7334ba0d467b77233bf37
clinton <clinton.wambua@student.moringaschool.com>
2023-07-04 09:25:35 +0300

    updates

--- client/src/components/home /Carousel.js
+++ client/src/components/home /Carousel.js
@@ -1,12 +1,9 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Carousel() {
   return (
-    <div
-      id="hero-carousel"
-      class="carousel slide"
-      data-bs-ride="carousel"
-    >
+    <div id="hero-carousel" class="carousel slide" data-bs-ride="carousel">
       <div class="carousel-inner">
         <div class="carousel-item active c-item" data-bs-interval="2000">
           <img
@@ -14,6 +11,14 @@ function Carousel() {
             class="d-block w-100 c-img"
             alt="slide 1"
           />
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Visit The Maasai Mara</h5>
+            <Link to="destinations">
+              <button type="button" class="btn btn-outline-dark">
+                Show more
+              </button>
+            </Link>
+          </div>
         </div>
         <div class="carousel-item c-item" data-bs-interval="2000">
           <img
@@ -21,27 +26,59 @@ function Carousel() {
             class="d-block w-100 c-img"
             alt="slide 2"
           />
+          <div class="carousel-caption d-none d-md-block">
+            <h5>See The Drylands of Kenya</h5>
+            <Link to="destinations">
+              <button type="button" class="btn btn-outline-dark">
+                Show more
+              </button>
+            </Link>
+          </div>
         </div>
-        <div class="carousel-item c-item">
+        <div class="carousel-item c-item" data-bs-interval="2000">
           <img
             src="https://images.unsplash.com/photo-1559494487-a5bbc635ed2b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8a2VueWElMjB3aWxkbGlmZSUyMGNhcnN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"
             class="d-block w-100 c-img"
             alt="slide 3"
           />
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Explore The Coolest City of Africa</h5>
+            <Link to="destinations">
+              <button type="button" class="btn btn-outline-dark">
+                Show more
+              </button>
+            </Link>
+          </div>
         </div>
-        <div class="carousel-item c-item">
+        <div class="carousel-item c-item" data-bs-interval="2000">
           <img
             src="https://plus.unsplash.com/premium_photo-1661846340419-89bf27138124?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8a2VueWElMjBmbGFtaW5nb3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60"
             class="d-block w-100 c-img"
             alt="slide 3"
           />
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Experience The Pink Carpet</h5>
+            <Link to="destinations">
+              <button type="button" class="btn btn-outline-dark">
+                Show more
+              </button>
+            </Link>
+          </div>
         </div>
-        <div class="carousel-item c-item">
+        <div class="carousel-item c-item" data-bs-interval="2000">
           <img
             src="https://images.unsplash.com/photo-1569474028404-5eb46fd9b4fa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGtlbnlhJTIwYmVhY2hlc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60"
             class="d-block w-100 c-img"
             alt="slide 3"
           />
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Chill and Dine On The Beaches</h5>
+            <Link to="destinations">
+              <button type="button" class="btn btn-outline-dark">
+                Show more
+              </button>
+            </Link>
+          </div>
         </div>
       </div>
       <button
