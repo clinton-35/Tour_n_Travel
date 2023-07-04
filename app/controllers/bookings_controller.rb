3ab9e1f82b3a59d0f213896f5bf9850fa40b139b
trevormadaramoringaschool <trevor.madara@student.moringaschool.gcom>
2023-07-04 16:24:32 +0300

    Done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,11 +1,10 @@
 class BookingsController < ApplicationController
+
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
     def create
-      booking = Booking.new(booking_params)
-      if booking.save
-        render json: { message: 'Booking submitted successfully' }, status: :created
-      else
-        render json: { error: 'Booking submission failed' }, status: :unprocessable_entity
-      end
+      booking = Booking.create!(booking_params)
+      render json: booking, status: :created
     end
   
     private
@@ -13,6 +12,9 @@ class BookingsController < ApplicationController
     def booking_params
       params.permit(:first_name, :last_name, :email, :phone, :check_in_date, :check_out_date, :adults, :children, :trip_preference)
     end
-  end
 
+    def render_unprocessable_entity_response(invalid)
+      render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
   
