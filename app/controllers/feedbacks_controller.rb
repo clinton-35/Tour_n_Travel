f99c068f1e5b6d309422b8aeba28c63aead1fbcb
Cheruiyot Kipkemoi <d.cheru112@gmail.com>
2023-07-07 11:39:29 +0300

    back end fix for feedback

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,52 +1,54 @@
 class FeedbacksController < ApplicationController
-  # before_action :set_feedback, only: [:show, :update, :destroy]
-  # skip_action :set_user, only: [:index, :create]
+  before_action :set_feedback, only: [:show, :update, :destroy]
+  before_action :set_user, only: [:index, :create]
 
   # GET /feedbacks
   def index
-      feedbacks = Feedback.all
-      render json: feedbacks
+    feedbacks = Feedback.all
+    render json: feedbacks
   end
 
   # GET /feedbacks/1
   def show
-    feedback = find_feedback
-    render json: feedback
+    render json: @feedback
   end
 
   # POST /feedbacks
   def create
-    feedback = Feedback.create(feedback_params)
-    render json: feedback, status: :created
+    feedback = Feedback.new(feedback_params)
+  
+    if feedback.save
+      render json: { success: "Feedback created successfully!" }, status: :created
+    else
+      render json: { error: "Failed to create feedback" }, status: :unprocessable_entity
+    end
   end
-
-  # PATCH/PUT /feedback/1
+  
+  # PATCH/PUT /feedbacks/1
   def update
-    feedback = find_feedback
-    feedback.update!(feedback_params)
-    render json: feedback
+    if @feedback.update(feedback_params)
+      render json: { success: "Feedback updated successfully!" }
+    else
+      render json: { error: "Failed to update feedback" }, status: :unprocessable_entity
+    end
   end
-
-  # DELETE /recipes/1
+  # DELETE /feedbacks/1
   def destroy
-    destination = find_destination
-    destination.destroy
-    render json: { success: "Destination was deleted successfully" }
+    @feedback.destroy
+    render json: { success: "Feedback was deleted successfully" }
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
-  def find_feedback
-    Feedback.find(params[:id])
+  def set_feedback
+    @feedback = Feedback.find(params[:id])
   end
 
   def set_user
     @user = User.find_by(id: session[:user_id]) if session[:user_id]
   end
 
-  # Only allow a list of trusted parameters through.
   def feedback_params
-    params.permit(:name, :service_usage, :feedback_text)
+    params.require(:feedback).permit(:name, :service_usage, :feedback_text)
   end
 end
